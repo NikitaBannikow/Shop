659ae78a0eacfb99fa3ee3af7617a12e2fcf1de0
NikitaBannikow <niikitaban@gmail.com>
2024-05-19 23:50:51 +0300

    0.5

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Items from "./Components/Items";
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       items: [
         {
           id: 1,
-          title: 'fw',
-          img: './img/burilka.jpg',
+          title: 'Бутылка',
+          img: "blini.jpg",
           desc: 'Хранит воду',
           category: 'Accessories',
           price: '1250.00'
         },
         {
           id: 2,
-          title: 'ref',
-          img: './img/kovrik.jpg',
+          title: 'Коврик',
+          img: "kovrik.jpg",
+          desc: 'На нем можно лежать',
+          category: 'Accessories',
+          price: '1500.00'
+        },
+        {
+          id: 3,
+          title: 'Коврик',
+          img: "kovrik.jpg",
           desc: 'На нем можно лежать',
           category: 'Accessories',
           price: '1500.00'
@@ -32,7 +40,7 @@ class App extends React.Component() {
     return (
       <div className="wrapper">
         <Header />
-        <Items items={this.state.items} />
+        <Items items = {this.state.items} />
         <Footer />
       </div> )
   }
