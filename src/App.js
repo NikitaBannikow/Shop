263bd1f004e7e0ff2f020069934ddc9aac93b275
NikitaBannikow <niikitaban@gmail.com>
2024-05-19 22:32:50 +0300

    modified:   src/App.js 	new file:   src/Components/Items.js 	new file:   src/img/butilka.jpg 	new file:   src/img/kovrik.jpg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Items from "./Components/Items";
 
 class App extends React.Component() {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       items: [
         {
-          
+          id: 1,
+          title: 'fw',
+          img: './img/burilka.jpg',
+          desc: 'Хранит воду',
+          category: 'Accessories',
+          price: '1250.00'
+        },
+        {
+          id: 2,
+          title: 'ref',
+          img: './img/kovrik.jpg',
+          desc: 'На нем можно лежать',
+          category: 'Accessories',
+          price: '1500.00'
         }
       ]
     }
   }
+  
   render() {
     return (
-    <div className="wrapper">
-      <Header/>
-      <Footer/>
-      </div>
-    );
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div> )
   }
 }
 
